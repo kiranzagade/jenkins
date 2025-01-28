1f3b325ee726f12d1e4cfc8b0329ffe1c4a812c8
kiranzagade <kiranzagade3181@gmail.com>
2025-01-28 10:29:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, Devops Team From Skills IT \n');
+  res.end('Hello, Devops Team From KIRAN ZAGADE \n');
 });
 
 server.listen(PORT, () => {
